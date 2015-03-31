1ca1ed3d54e3a629c95ffdc9b0eca783f00bc6e4
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2015-03-31 10:57:38 +0000

    cleaned up package.json

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors')
-var cookieParser = require('cookie-parser')
 var config = require('../config.json');
 var server = app.listen(80, function () {
       console.log('p2p listening');
@@ -13,7 +12,5 @@ function log(req, res, next) {
 }
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
-//app.use(bodyParser.json());
-app.use(cookieParser());
 app.use(log);
 
